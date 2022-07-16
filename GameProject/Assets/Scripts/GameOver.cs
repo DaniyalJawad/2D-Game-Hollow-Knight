0ed2be7193050ff8415a63801afdf8e30a780878
Daniyal <F2020266323@umt.edu.pk>
2022-07-16 08:16:45 -0700

    2D Game

---
+++ GameProject/Assets/Scripts/GameOver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public static event Action onPlayerDeath;
+    public void Game_Over()
+    {
+        if(PlayerHealth.currentHealth<=0)
+        {
+            onPlayerDeath?.Invoke();
+        }
+    }
+}
---
+++ GameProject/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Button[] levelButton;
+
+    private void Start()
+    {
+        foreach (Button b in levelButton)
+            b.interactable = false;
+        int reachedLevel = PlayerPrefs.GetInt("ReachedLevel", 1);
+
+        for (int i = 0; i < reachedLevel; i++)
+            levelButton[i].interactable = true;
+    }
+
+}
